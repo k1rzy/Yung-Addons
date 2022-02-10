da24da7deb44eacac89aaf51bf38f69c56ee057f
encodey <54742240+encodey@users.noreply.github.com>
2022-02-10 18:16:06 +0300

    Add files via upload

---
+++ src/main/java/com/encodey/YungAddons/Core/Feature.java
@@ -0,0 +1,15 @@
+package com.encodey.YungAddons.Core;
+
+import com.encodey.YungAddons.features.Mining.CrystalHollows.WormNotifier;
+import com.encodey.YungAddons.lib.ColorCode;
+import com.encodey.YungAddons.lib.GuiFeatureData;
+import com.google.common.collect.Sets;
+import lombok.Getter;
+import net.minecraft.client.Minecraft;
+
+import java.util.*;
+
+@Getter
+public enum Feature {;
+    private static final Set<Feature> SETTINGS = Sets.newHashSet();
+}
