fa646f81e2579ebb3d2454a44d2dc5ce90593cae
k1rzy <54742240+k1rzy@users.noreply.github.com>
2022-02-11 17:40:40 +0300

    Add files via upload

---
+++ src/main/java/com/encodey/YungAddons/lib/LibResources.java
@@ -0,0 +1,11 @@
+package com.encodey.YungAddons.lib;
+
+public class LibResources {
+
+    public static final String PREFIX_MOD = "yungaddons:";
+    public static final String PREFIX_GUI = PREFIX_MOD + "textures/gui/";
+    public static final String PREFIX_SPELL = PREFIX_MOD + "textures/spell/";
+    public static final String PREFIX_MODEL = PREFIX_MOD + "textures/model/";
+    public static final String PREFIX_MISC = PREFIX_MOD + "textures/misc/";
+
+}
